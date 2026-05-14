f87de16fd296b09b14487b77acf85f4053bd015e
Your Name <you@example.com>
2026-05-14 20:21:28 +0000

    new try

--- graph.c
+++ graph.c
@@ -56,9 +56,9 @@ int getWeight(Graph* g, const char* label1, const char* label2) {
         Edge * aux = list_first(lista);
         while(aux != NULL){
 
-            if (is_equal_string(aux->target, (char *)label2) == 0) return aux->weight;
+            if (is_equal_string(aux->target, (char *)label2)) return aux->weight;
 
-            next_list(pair->value);
+            list_next(pair->value);
         }
     }
     // Si no existe el origen o terminamos de iterar sin encontrar el destino
