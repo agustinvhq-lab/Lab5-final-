0131e9a3df83c25934eb90b4f0dd2d71a2d68a94
Your Name <you@example.com>
2026-05-16 17:07:48 +0000

    new try

--- graph.c
+++ graph.c
@@ -50,7 +50,7 @@ void addEdge(Graph* g, const char* src, const char* dest, int weight) {
     MapPair* pair = map_search(g->adjacencyMap, (char*)src);
     if (pair == NULL) return;
     
-    Edge aux;
+    Edge* aux;
     aux->weight = weight;
     strcpy(aux->target, dest);
 
