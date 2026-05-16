3826a6c1530ec0c410ceb7962cc3466e0762c08c
Your Name <you@example.com>
2026-05-16 17:32:43 +0000

    new try

--- graph.c
+++ graph.c
@@ -52,7 +52,7 @@ void addEdge(Graph* g, const char* src, const char* dest, int weight) {
     
     Edge* aux = (Edge*)malloc(sizeof(Edge));
     aux->weight = weight;
-    aux->target = (char*)malloc(sizeof(dest) + 1);
+    aux->target = (char*)malloc(sizeof(char * (strlen(dest) + 1));
     strcpy(aux->target, dest);
 
     list_pushCurrent(pair->value, aux);
